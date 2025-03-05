e2795370c2e24f51808486ce214a2722e512e4a6
BigBinary <bigbinary@BigBinarys-MacBook-Pro-2.local>
2025-03-05 15:30:50 +0530

    Added a user model

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,14 +3,23 @@
 class Task < ApplicationRecord
   MAX_TITLE_LENGTH = 125
   VALID_TITLE_REGEX = /\A.*[a-zA-Z0-9].*\z/i
-
   validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }, format: { with: VALID_TITLE_REGEX }
   validates :slug, uniqueness: true
   validate :slug_not_changed
+  before_validation :set_title
+  before_save :change_title
 
   before_create :set_slug
 
   private
+
+    def set_title
+      self.title = "Pay electricity bill"
+    end
+
+    def change_title
+      self.title = "Pay electricity & TV bill"
+    end
 
     def set_slug
       title_slug = title.parameterize
