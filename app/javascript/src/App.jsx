dee76f347f2d421bc9275257fb7771ba20fa3370
Amit Kumar <mohapatraamitkumar450@gmail.com>
2025-09-09 00:25:36 +0530

    Added signup feature

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -4,6 +4,7 @@ import { CreateTask, ShowTask, EditTask } from "components/Tasks";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
+import Signup from "./components/Authentication/Signup";
 import Dashboard from "./components/Dashboard";
 
 const App = () => (
@@ -14,6 +15,7 @@ const App = () => (
       <Route exact component={ShowTask} path="/tasks/:slug/show" />
       <Route exact component={CreateTask} path="/tasks/create" />
       <Route exact component={Dashboard} path="/dashboard" />
+      <Route exact component={Signup} path="/signup" />
     </Switch>
   </Router>
 );
