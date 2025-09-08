8102137569339e4d92d19c53db608ea5d59428f2
Amit Kumar <mohapatraamitkumar450@gmail.com>
2025-09-08 21:58:46 +0530

    Added ability to assign task to a user

--- app/javascript/src/components/Tasks/Table/Row.jsx
+++ app/javascript/src/components/Tasks/Table/Row.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Tooltip } from "components/commons";
 import PropTypes from "prop-types";
 
-const Row = ({ data, destroyTask, showTask }) => (
+const Row = ({ data, showTask, destroyTask }) => (
   <tbody className="divide-y divide-gray-200 bg-white">
     {data.map(rowData => (
       <tr key={rowData.id}>
@@ -11,6 +11,9 @@ const Row = ({ data, destroyTask, showTask }) => (
           <Tooltip tooltipContent={rowData.title}>
             <span>{rowData.title}</span>
           </Tooltip>
+        </td>
+        <td className="whitespace-no-wrap border-r border-gray-300 px-4 py-2.5 text-sm text-gray-800">
+          {rowData.assigned_user.name}
         </td>
         <td className="cursor-pointer px-6 py-4 text-right text-sm font-medium leading-5">
           <a className="text-indigo-600" onClick={() => showTask(rowData.slug)}>
