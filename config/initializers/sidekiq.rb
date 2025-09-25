fc77a8f102d34622e0c7ae10ad6bb317f4489aac
Amit Kumar <mohapatraamitkumar450@gmail.com>
2025-09-25 21:49:24 +0530

    Disabled redis if redis_url not present

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,17 +7,21 @@ if Rails.env.test?
   Sidekiq::Testing.inline!
 end
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: Rails.application.secrets.redis_url, size: 9 }
-  unless Rails.env.test? || Rails.env.production?
-    schedule_file = "config/scheduled_jobs.yml"
+if ENV["REDIS_URL"].present?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: Rails.application.secrets.redis_url, size: 9 }
+    unless Rails.env.test? || Rails.env.production?
+      schedule_file = "config/scheduled_jobs.yml"
 
-    if File.exist?(schedule_file)
-      Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file, aliases: true)[Rails.env]
+      if File.exist?(schedule_file)
+        Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file, aliases: true)[Rails.env]
+      end
     end
   end
-end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: Rails.application.secrets.redis_url, size: 1 }
+  Sidekiq.configure_client do |config|
+    config.redis = { url: Rails.application.secrets.redis_url, size: 1 }
+  end
+else
+  Rails.application.config.active_job.queue_adapter = :inline
 end
